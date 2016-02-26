99dc23b9b70f5e0b77ec7ac801c508b99673edb4
승원 <milk5590@naver.com>
2016-02-26 14:10:49 +0900

    간단한 문구 추가

--- src/main/java/milk/revivalblock/RevivalBlock.java
+++ src/main/java/milk/revivalblock/RevivalBlock.java
@@ -1,17 +1,18 @@
 package milk.revivalblock;
 
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.TextFormat;
 
 public class RevivalBlock extends PluginBase{
 
     @Override
     public void onEnable(){
-
+        this.getServer().getLogger().info(TextFormat.GOLD + "[RevivalBlock]Plugin has been enabled");
     }
 
     @Override
     public void onDisable(){
-
+        this.getServer().getLogger().info(TextFormat.GOLD + "[RevivalBlock]Plugin has been disabled");
     }
 
 }
